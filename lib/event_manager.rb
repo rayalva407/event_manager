558869fae975b8fbb90fea6792cf831af4faf5fe
Ray <rayalva407@gmail.com>
2024-05-03 21:45:44 -0600

    prints the all first names in csv file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,7 +3,8 @@ puts 'Event Manager Initialized!'
 
 lines = File.readlines('event_attendees.csv')
 
-lines.each do |line|
+lines.each_with_index do |line, index |
+  next if index == 0
   columns = line.split(',')
   name = columns[2]
   puts name
