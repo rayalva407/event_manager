08ee44852cffdddf88fdc1c431d45776dc12277f
Ray <rayalva407@gmail.com>
2024-05-03 22:05:05 -0600

    implements header_converters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,8 +2,14 @@ require 'csv'
 
 puts 'Event Manager Initialized!'
 
-contents = CSV.open('event_attendees.csv', headers: true)
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
 contents.each do |row|
-  name = row[2]
-  puts name
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
 end
